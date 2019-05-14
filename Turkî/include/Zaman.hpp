602786931c6f83188809f4e0dc78f372e67c325d
Üsküdarlı Fikret oğlu Muhammed Şamil <muhammed.garib@gmail.com>
2019-05-14 04:35:06 +0300

    yüklendi

---
+++ Turkî/include/Zaman.hpp
@@ -0,0 +1,46 @@
+#ifndef Zaman_h
+#define Zaman_h
+
+#include <iostream>
+#include <string>
+#include <ctime>
+#include "pugixml.hpp"
+
+
+class Zaman
+{
+public:
+	pugi::xml_document dosya;
+	pugi::xml_node sehir;
+
+	std::string bu_gun_deger;
+	std::string xml_bu_gun;
+	std::string b_h_aksam;
+	std::string simdiki_zaman_turk;
+
+	time_t now;
+	tm *ltm;
+
+	const char* dosya_adresi;
+	const char* bu_gun_const;
+
+	char* dt;
+
+	unsigned int hrist_rakam_gun_senenin;
+	unsigned int saat;
+	unsigned int dakika;
+	unsigned int saniye;
+	unsigned int hrist_saat;
+	unsigned int hrist_dakika;
+	unsigned int hrist_saniye;
+
+
+	void tkvm_hrist_v_d();
+	void vkt_karisik_gecici();
+	void sat_hrist_v_d();
+
+	Zaman();
+	~Zaman();
+};
+
+#endif
---
+++ Turkî/src/main.cpp
@@ -0,0 +1,9 @@
+#include "Zaman.hpp"
+
+int main()
+{
+	Zaman baslat;
+	std::cout<<"cikmak icin enter'a basiniz"<<std::endl;
+	std::cin.get();
+	return 0;
+}
